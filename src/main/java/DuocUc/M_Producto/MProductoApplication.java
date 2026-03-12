eae1267593ec7c89b15eabfbfa46126a0d687c30
alejandro montecinos <alejandromontecino002@gmail.com>
2026-03-12 17:15:42 -0300

    se agregar archivos principales para api

---
+++ src/main/java/DuocUc/M_Producto/MProductoApplication.java
@@ -0,0 +1,13 @@
+package DuocUc.M_Producto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MProductoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MProductoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/DuocUc/M_Producto/MProductoApplicationTests.java
@@ -0,0 +1,13 @@
+package DuocUc.M_Producto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MProductoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
